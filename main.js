d23d670458c9d8b585368bb6ccdba991c2bd46df
Le Hoang Loi <nhoxac94@yahoo.com>
2021-05-23 21:55:47 +0700

    add program in webside

--- main.js
+++ main.js
@@ -1,41 +1,69 @@
-// Bai 4: Tinh dien tich, chu vi hinh chu nhat 
+// Bai 4: Tính diện tích và chu vi hình chữ nhật
 /**
- * B1: Input: Chieu dai, chieu rong hinh chu nhat
+ * B1: Input: Chiều dài và chiều rộng hình chứ nhật.
  * B2:
- * - Dat bien chieuDai, chieuRong, chuVi, dienTich
- * - Tinh chuVi = (chieuDai + chieuRong)*2
- * - Tinh dienTich = (chieuDai * chieuRong)
+ * - Tạo form để người dùng nhập chiều dài và chiều rộng, nút tính kết quả, nơi xuất kết quả.
+ * - Dat bien  chieuDaiHinhChuNhat, chieuRongHinhChuNhat để lấy giá trị người dùng nhập.
+ * - Tính giá trị khí người dùng click vào button:
+ *  + tinhChuVi = (chieuDai + chieuRong)*2
+ *  + tinhDienTich = (chieuDai * chieuRong)
+ * 
  * B3:
- * - Xuat ket qua chuVi va dienTich
+ * - Xuat ket qua tinhChuVi va tinhDienTich ra màn hình.
  */
 
 console.log('===================Bai4===============');
-var chieuDai = 5;
-var chieuRong = 10;
-var chuVi, dienTich;
-chuVi = (chieuDai + chieuRong) * 2;
-dienTich = (chieuDai * chieuRong);
-console.log('Chu vi hinh chu nhat la: ',chuVi);
-console.log('Dien tich hinh chu nhat la: ',dienTich);
+
+
+var btnKetQua = document.getElementById('btnKetQua');
+var chuViHinhChuNhat = document.getElementById('chuViHinhChuNhat');
+var dienTichHinhChuNhat = document.getElementById('dienTichHinhChuNhat');
+var ketQua = document.getElementById('ketQua');
+
+ketQua.style.display = 'none';
+
+btnKetQua.onclick = function() {
+    var chieuDaiHinhChuNhat = Number(document.getElementById('chieuDaiHinhChuNhat').value);
+    var chieuRongHinhChuNhat = Number(document.getElementById('chieuRongHinhChuNhat').value);
+    var tinhChuVi = (chieuDaiHinhChuNhat + chieuRongHinhChuNhat) * 2;
+    var tinhDienTich = chieuDaiHinhChuNhat * chieuRongHinhChuNhat;
+   
+    ketQua.style.display = 'block';
+    chuViHinhChuNhat.innerHTML = 'Chu vi hình chữ nhật là: '+ tinhChuVi +' cm^2';
+    dienTichHinhChuNhat.innerHTML = 'Diện tích hình chữ nhật là: '+ tinhDienTich +' cm^3';
+    
+   
+}
+
+
+
 
 
 //Bai 5: Tong 2 ki tu cua so
 /**
  * B1: Input : so co 2 chu so
  * B2: Handle
- * - Dat bien: soHaiChuSo, soHangDonVi, soHangChuc, tongHaiSo
- * - Tinh: 
+ * - Tạo form để người dùng nhập số, nút button, vùng xuất dữ liệu.
+ * - Dat bien: soHaiChuSo để lấy giá trị người dùng nhập
+ * - Dặt biến: soHangDonVi, soHangChuc, tongHaiSo.
+ * - Tinh khi người dùng click vào button. 
  *  + soHangDonVi = soHaiChuSo % 10;
  *  + soHangChuc = (sohaiChuSo - soHangDonVi )/10
- *  + tongHaiSo = soHangDonVi + soHangChuc
+ *  + tongDonViVaChuc = soHangDonVi + soHangChuc
  * B3:Output
- * - Xuat ket qua tongHaiSo
+ * - Xuat ket qua tongDonViVaChuc ra màn hình.
  */
 
  console.log('===================Bai5===============');
-var soHaiChuSo = 99;
-var soHangDonVi, soHangChuc, tongHaiSo;
-soHangDonVi = soHaiChuSo % 10;
-soHangChuc = (soHaiChuSo - soHangDonVi)/10;
-tongHaiSo = soHangChuc + soHangDonVi;
-console.log('Tong hai chu so cua', soHaiChuSo,'la: ',tongHaiSo);
+
+ var btnTinhTong = document.getElementById('btnTinhTong');
+ var ketQuaTong = document.getElementById('btnTinhTong');
+ btnTinhTong.onclick = function() {
+    var soCoHaiChuSo = document.getElementById('soCoHaiChuSo').value;
+    var soHangDonVi = soCoHaiChuSo % 10;
+    var soHangChuc = (soCoHaiChuSo - soHangDonVi)/10;
+    var tongDonViVaChuc = soHangDonVi + soHangChuc ;
+
+    ketquaTong.innerHTML = 'Tổng hai số của số ' +  soCoHaiChuSo + ' là ' + tongDonViVaChuc;
+ }
+
